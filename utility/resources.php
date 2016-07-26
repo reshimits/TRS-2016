44cb6e0fc1c0cdcef56d5f1d9c5f0fa5a407b4f7
Rob Shimits | reshimits <r.e@shimits.com>
2016-07-26 17:23:04 -0400

    Resources page(s) progress

---
+++ utility/resources.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<head>
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>Resources Hub // THE RETROFIT SOURCE</title>
+    <meta name="description" content="Google Description Here">
+    <meta name="keywords" content="Keywords Here" />
+    <meta name="author" content="SHIMITS Design/Development">
+
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="format-detection" content="telephone=no" />
+
+    <!-- CSS
+    ================================================== -->
+    <link href="../css/bootstrap-theme-full.css" rel="stylesheet">
+    <link href="../css/trs-theme.css" rel="stylesheet">
+
+    <link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+    
+    <!-- Javascript [primary]
+    ================================================== -->
+    
+    
+    <!-- Favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="../img/favicon.ico">
+    <link rel="icon" sizes="32x32" href="../img/favicon-32.png">
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="../img/favicon-152.png">
+
+</head>
+
+<body class="utility resources">
+
+    <!-- Header
+    ================================================== -->
+
+    <?php require('../shared/header.html');?>
+
+
+    <!-- Primary Page Layout
+    ==================================================
+    ==================================================
+    ================================================== -->
+
+    <div class="bodyWrap">
+        
+        <div class="jumbotron wLinks">
+            <div class="featureHeadline col-xs-12">
+                <div class="col-xs-12 col-sm-8">
+                    <h1>Resources: <span class="text-weightRegular text-secondary">Hub</span></h1>
+                    <h5 class="text-white">Our hub of headlight happiness! Pro tips, user uploaded pics, headlight tech, and of course social media.</h5>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-3 pull-right">
+                    <a type="button" href="customerguides.php" class="btn btn-block btn-ghost">Install Guides</a>
+                    <a type="button" href="gallery.php" class="btn btn-block btn-ghost">Customer Gallery</a>
+                    <a type="button" href="tech.php" class="btn btn-block btn-ghost">Headlight Tech</a>
+                    <a type="button" href="https://www.hidplanet.com/forums/forum" target="_blank" class="btn btn-block btn-ghost">HID Planet Forum</a>
+                    <a type="button" href="https://www.facebook.com/groups/389976441138867/" target="_blank" class="btn btn-block btn-ghost">Headlight Junkies</a>
+                </div>
+            </div>
+            <div class="jumbotron-img"></div>
+        </div>
+
+        <div class="section wrap header-waypoint" data-animate-down="header-small" data-animate-up="header-large">
+            <div class="flex-row">
+                <div class="col-xs-12 col-sm-6 section blockquote1">
+                    <div class="container-fluid">
+                    <a href="customerguides.php">
+                        <div class="col-xs-12">
+                            <div class="section first">
+                            <div class="blockquote">
+                                <div class="btn btn-empty btn-hexicon"><span class="icon icon-resourcesHex-guides"></span></div>
+                                <h1>Install Guides</h1>
+                                <h6 class="text-white">Do it right, the first time. If you’ve got some parts and don’t know where to start, dig in. </h6>
+                            </div>
+                            </div>
+                        </div>  
+                    </a>
+                    </div>          
+                </div>
+
+                <div class="col-xs-12 col-sm-6 section gray2 blockquote2">
+                    <div class="container-fluid">
+                    <a href="tech.php">
+                        <div class="col-xs-12">
+                            <div class="section first">
+                            <div class="blockquote">
+                                <div class="btn btn-empty btn-hexicon"><span class="icon icon-resourcesHex-tech"></span></div>
+                                <h1>Headlight Tech</h1>
+                                <h6 class="text-white">Knowledge is power! Educate yourself and understand why retrofitting is the right thing to do.</h6>
+                            </div>
+                            </div>
+                        </div>  
+                    </a>
+                    </div>          
+                </div>
+            </div>
+
+            <div class="section promo promo1">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="caption col-sm-10 col-md-8 col-lg-7 pull-right">
+                            <div class="btn btn-empty btn-hexicon"><span class="icon icon-resourcesHex-gallery"></span></div>
+                            <h1 class="productdetail">Customer Gallery</h1>
+                            <h6 class="text-white text-shadow">See the light! literally. Get some inspiration and check out our collection of customer cars rocking TRS products. Then join the club and upload your own! </h6>
+                            <a type="button" href="gallery.php" class="btn btn-ghost">Enter Gallery</a>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+
+            <div class="section wrap">
+            <div class="flex-row">
+                <div class="col-xs-12 col-sm-6 section gray3 blockquote3">
+                    <div class="container-fluid">
+                    <a href="https://www.hidplanet.com/forums/forum" target="_blank">
+                        <div class="col-xs-12">
+                            <div class="section first">
+                            <div class="blockquote">
+                                <div class="btn btn-empty btn-hexicon"><span class="icon icon-resourcesHex-hidplanet"></span></div>
+                                <h1>HID Planet Forum</h1>
+                                <h6 class="text-white">The internet’s most knowledgable crowd of headlight enthusiasts in one place.</h6>
+                            </div>
+                            </div>
+                        </div>  
+                    </a>
+                    </div>          
+                </div>
+
+                <div class="col-xs-12 col-sm-6 section gray1 blockquote4">
+                    <div class="container-fluid">
+                    <a href="https://www.facebook.com/groups/389976441138867/" target="_blank">
+                        <div class="col-xs-12">
+                            <div class="section first">
+                            <div class="blockquote">
+                                <div class="btn btn-empty btn-hexicon"><span class="icon icon-resourcesHex-hj"></span></div>
+                                <h1>Headlight Junkies Group</h1>
+                                <h6 class="text-white">If social media is your thing and lighting is your current fling, join the club!</h6>
+                            </div>
+                            </div>
+                        </div>  
+                    </a>
+                    </div>          
+                </div>
+            </div>
+            </div>
+
+        </div>
+
+        
+
+        <!-- Footer
+        ================================================== -->
+
+        <?php require('../shared/footer.html');?>
+
+
+    </div> <!-- /container -->
+
+    <?php require('../shared/modals.html');?>
+
+
+    <!-- End Primary Page Layout
+    ==================================================
+    ==================================================
+    ================================================== -->
+        
+    
+
+    <!-- Javascript [secondary]
+    ================================================== -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>   
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script> 
+
+    <!-- HEADER/NAV size change based on scroll -->
+    <script src="../js/waypoints.min.js"></script>
+    <script>
+        var $head = $( '#header' );
+        $( '.header-waypoint' ).each( function(i) {
+            var $el = $( this ),
+                animClassDown = $el.data( 'animateDown' ),
+                animClassUp = $el.data( 'animateUp' );
+
+            $el.waypoint( function( direction ) {
+                if( direction === 'down' && animClassDown ) {
+                    $head.attr('class', 'header ' + animClassDown);
+                }
+                else if( direction === 'up' && animClassUp ){
+                    $head.attr('class', 'header ' + animClassUp);
+                }
+            }, { offset: '0%' } );
+        } );
+    </script>
+
+    <!-- HEADER/NAV toggle MENU BTN -->
+    <script type='text/javascript'>
+        function toggleText(id) {
+            var showMore = document.getElementById(id);
+            (showMore.style.display=='block') ? showMore.style.display='none' : showMore.style.display='block' ;  
+        }
+        $( ".navbar-menu" ).click(function() {
+            $(".navbar-menu").toggleClass( "active" );
+            $(".bodyWrap").toggleClass( "active" );
+        });
+    </script>
+
+    <!-- iOS Viewport Units Buggyfill -->
+    <script src="../js/viewport-units-buggyfill.js"></script>
+    <script>window.viewportUnitsBuggyfill.init();
+        var hacks = require('viewport-units-buggyfill.hacks');
+        require('viewport-units-buggyfill').init({
+          hacks: hacks
+        });
+    </script>
+
+  
+
+    <!-- End Document
+    ================================================== -->
+
+</body>
+</html>
