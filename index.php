0a60b7a54a43d74ca13ee1665c87dbda39cbc768
Rob Shimits | reshimits <r.e@shimits.com>
2018-11-12 13:29:19 -0500

    NEW NAV and JUMBO

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
                 <div class="jumbotron-img">
                     <video id="video-background" autoplay loop muted plays-inline>
                         <!-- <source src="https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761" type="video/mp4"> -->
-                        <source src="img/feature2018-IMG_1131.mov" type="video/mp4">   
+                        <source src="img/feature2018-video1.mp4" type="video/mp4">   
                     </video>
                 </div>
                 <div class="jumbotron-honeycomb"></div>
